163ee4d25955fedc2994dae7d03c5bdb334f3812
Mehmet Konukcu <mehmetext68@gmail.com>
2022-09-12 00:57:57 +0300

    Seeding part for Product and Review

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Model\Product;
+use App\Models\Model\Review;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +18,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        Product::factory(50)->create();
+        Review::factory(300)->create();
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
