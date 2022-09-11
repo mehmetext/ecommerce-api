072f203f148cb30dc939de8eb49ac2545d497783
Mehmet Konukcu <mehmetext68@gmail.com>
2022-09-12 01:48:52 +0300

    product collection resource

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -14,6 +14,15 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return $this->collection->map(function ($item) {
+            return [
+                "name" => $item->name,
+                "totalPrice" => round((1 - $item->discount / 100) * $item->price, 2),
+                "rating" => $item->reviews->count() > 0 ? (round($item->reviews->sum("star") / $item->reviews->count(), 2)) : 0,
+                "href" => [
+                    "link" => route("products.show", $item->id),
+                ],
+            ];
+        });
     }
 }
