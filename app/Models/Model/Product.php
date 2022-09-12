4f078b75fe638c806f788ab01d187b2e110c95d0
Mehmet Konukcu <mehmetext68@gmail.com>
2022-09-12 21:09:07 +0300

    Product update

--- app/Models/Model/Product.php
+++ app/Models/Model/Product.php
@@ -9,6 +9,8 @@ class Product extends Model
 {
     use HasFactory;
 
+    protected $fillable = ["name", "detail", "stock", "price", "discount"];
+
     protected $casts = array(
         "discount" => "integer",
         "stock" => "integer",
